6638bf8cdc94377331ac3a3ae14e708b13ca4969
jskenigson <jessica.kenigson@colorado.edu>
2021-07-05 17:06:34 -0600

    Fix misplaced comma, ampersand

--- src/parameterizations/lateral/MOM_thickness_diffuse.F90
+++ src/parameterizations/lateral/MOM_thickness_diffuse.F90
@@ -606,8 +606,8 @@ subroutine thickness_diffuse_full(h, e, Kh_u, Kh_v, tv, uhD, vhD, cg1, dt, G, GV
   real, dimension(SZI_(G)) :: &
     drho_dT_v, &  ! The derivative of density with temperature at v points [R degC-1 ~> kg m-3 degC-1]
     drho_dS_v, &  ! The derivative of density with salinity at v points [R ppt-1 ~> kg m-3 ppt-1].
-    drho_dT_dT_h ! The second derivative of density with temperature at h points [R degC-2 ~> kg m-3 degC-2]	
-    drho_dT_dT_hr! The second derivative of density with temperature at h (+1) points [R degC-2 ~> kg m-3 degC-2]	
+    drho_dT_dT_h, & ! The second derivative of density with temperature at h points [R degC-2 ~> kg m-3 degC-2]	
+    drho_dT_dT_hr ! The second derivative of density with temperature at h (+1) points [R degC-2 ~> kg m-3 degC-2]	
   real :: uhtot(SZIB_(G), SZJ_(G))  ! The vertical sum of uhD [H L2 T-1 ~> m3 s-1 or kg s-1].
   real :: vhtot(SZI_(G), SZJB_(G))  ! The vertical sum of vhD [H L2 T-1 ~> m3 s-1 or kg s-1].
   real, dimension(SZIB_(G)) :: &
@@ -624,7 +624,7 @@ subroutine thickness_diffuse_full(h, e, Kh_u, Kh_v, tv, uhD, vhD, cg1, dt, G, GV
     pres_h, &     ! Pressure on the interface at the h-point [R L2 T-2 ~> Pa].
     T_hr, &       ! Temperature on the interface at the h (+1) point [degC].
     S_hr, &       ! Salinity on the interface at the h (+1) point [ppt].	
-    pres_hr &     ! Pressure on the interface at the h (+1) point [R L2 T-2 ~> Pa].
+    pres_hr       ! Pressure on the interface at the h (+1) point [R L2 T-2 ~> Pa].
   real :: Work_u(SZIB_(G), SZJ_(G)) ! The work being done by the thickness
   real :: Work_v(SZI_(G), SZJB_(G)) ! diffusion integrated over a cell [R Z L4 T-3  ~> W ]
   real :: Work_h        ! The work averaged over an h-cell [R Z L2 T-3 ~> W m-2].
