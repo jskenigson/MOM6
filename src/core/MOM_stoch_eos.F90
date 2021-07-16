4d4b9550cf7b69603adee484f7f4e6cb45b2db1d
jskenigson <jessica.kenigson@colorado.edu>
2021-07-16 11:08:51 -0600

    Reverted variable declarations

--- src/core/MOM_stoch_eos.F90
+++ src/core/MOM_stoch_eos.F90
@@ -162,10 +162,10 @@ contains
 ! locals
   integer                                ::  i,j,k
   real :: Tl(5)              ! copy and T in local stencil [degC]
-  real :: TX2                ! dx times dT/dx, squared [degC^2]
-  real :: TY2                ! dy times dT/dy, squared [degC^2]
+  real :: mn_T               ! mean of T in local stencil [degC]
+  real :: mn_T2              ! mean of T**2 in local stencil [degC]
   real :: hl(5)              ! Copy of local stencil of H [H ~> m]
-  real :: r_sm_H             ! Reciprocal of sum of H in local stencil [H-3 ~> m-3]
+  real :: r_sm_H             ! Reciprocal of sum of H in local stencil [H-1 ~> m-1]
 
   ! This block does a thickness weighted variance calculation and helps control for
   ! extreme gradients along layers which are vanished against topography. It is
